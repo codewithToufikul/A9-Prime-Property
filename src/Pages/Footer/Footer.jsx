47dc04432f8cd316f0d6e9108cd8a1b1a0f861ad
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-04-16 19:26:22 +0600

    website footer added

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -12,21 +12,22 @@ const Footer = () => {
           <img className=" mt-5" src={logo} alt="" />
         </div>
         <div className="p-5">
-          <div className="text-sm uppercase text-indigo-600 font-bold">Resources</div>
-          <a className="my-3 block" href="/#">Documentation <span className="text-teal-600 text-xs p-1"></span></a>
-          <a className="my-3 block" href="/#">Tutorials <span className="text-teal-600 text-xs p-1"></span></a>
-          <a className="my-3 block" href="/#">Support <span className="text-teal-600 text-xs p-1">New</span></a>
+          <div className="text-sm uppercase text-indigo-600 font-bold">Properties</div>
+          <a className="my-3 block" >Home For Sale <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Private Island <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Private Villas <span className="text-teal-600 text-xs p-1">New</span></a>
         </div>
         <div className="p-5">
           <div className="text-sm uppercase text-indigo-600 font-bold">Support</div>
-          <a className="my-3 block" href="/#">Help Center <span className="text-teal-600 text-xs p-1"></span></a>
-          <a className="my-3 block" href="/#">Privacy Policy <span className="text-teal-600 text-xs p-1"></span></a>
-          <a className="my-3 block" href="/#">Conditions <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Help Center <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Privacy Policy <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Conditions <span className="text-teal-600 text-xs p-1"></span></a>
         </div>
         <div className="p-5">
           <div className="text-sm uppercase text-indigo-600 font-bold">Contact us</div>
-          <a className="my-3 block" href="/#">Maple Avenue, Townsville, Floor 4 San Francisco, CA <span className="text-teal-600 text-xs p-1"></span></a>
-          <a className="my-3 block" href="/#">primepropety@yahoo.com <span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" href="/#">123 Ocean Drive, Miami, FL 33139<span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >Phone: xxx 555-6789<span className="text-teal-600 text-xs p-1"></span></a>
+          <a className="my-3 block" >primepropety@yahoo.com <span className="text-teal-600 text-xs p-1"></span></a>
         </div>
       </div>
     </div>
@@ -34,16 +35,16 @@ const Footer = () => {
     <div className="bg-gray-100 pt-2">
       <div className="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col max-w-screen-lg items-center">
         <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-          <a href="/#" className=" text-xl mx-1">
+          <a  className=" text-xl mx-1">
           <FaFacebook />
           </a>
-          <a href="/#" className=" text-xl mx-1">
+          <a  className=" text-xl mx-1">
             <FaInstagramSquare />
           </a>
-          <a href="/#" className=" text-xl mx-1">
+          <a  className=" text-xl mx-1">
             <FaGithub/>
           </a>
-          <a href="/#" className=" text-xl mx-1">
+          <a className=" text-xl mx-1">
             <FaTwitter />
           </a>
         </div>
